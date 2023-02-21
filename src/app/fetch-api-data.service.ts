ce3e30cc63c9fe8649356418da4c142a8eca9539
AlissaCoto89 <acoto189@gmail.com>
2023-02-21 16:01:01 -0500

    created registration form and login form

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import {
   HttpClient,
   HttpHeaders,
@@ -9,37 +9,38 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://my-flix-db-akc.herokuapp.com';
+const apiUrl = 'https://my-flix-db-akc.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   constructor(private http: HttpClient) {}
 
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
-      .post(`${apiUrl}/users`, userDetails)
-      .pipe(catchError(this.handleError));
-  }
+    .post(apiUrl + 'users', userDetails)
+    .pipe(catchError(this.handleError)
+      );
+    }
 
-  userLogin(userDetails: any): Observable<any> {
+  public userLogin(userDetails: any): Observable<any> {
     console.log(userDetails);
     return this.http
-      .post(`${apiUrl}/login`, userDetails)
-      .pipe(catchError(this.handleError));
-  }
+    .post(apiUrl + 'login', userDetails)
+    .pipe(catchError(this.handleError));
+}
 
   getAllMovies(): Observable<any> {
     const token = localStorage.getItem('token');
     return this.http
-      .get(`${apiUrl}/movies`, {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-      })
-      .pipe(map(this.extractResponseData), catchError(this.handleError));
-  }
+    .get(apiUrl + 'movies', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + token,
+      }),
+    })
+    .pipe(map(this.extractResponseData), catchError(this.handleError));
+}
 
   getMovie(title: string): Observable<any> {
     const token = localStorage.getItem('token');
